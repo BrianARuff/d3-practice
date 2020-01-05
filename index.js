a6d1390665c4598039a447c981caedc6a51a74ba
= <=>
2020-01-05 03:55:08 -0500

    d3 practice have basic understanding of filling in data in linear time using the data method with css styling and attributes like title so that you can see information about the given data

---
+++ index.js
@@ -0,0 +1,27 @@
+// d3.selectAll("p").transition("ease-in-out").duration(1000).style("color", "blue");
+
+function randomVal() {
+    const values = "0123456789abcdef";
+    const len = values.length;
+    const res = values[Math.floor(Math.random() * len)];
+
+    if (typeof res === "string") {
+        return res;
+    } else {
+        return String(res);
+    }
+}
+
+function num(min = 16, max = 36) {
+    const rangeArr = [...Array(36).splice(15)]
+    return rangeArr;
+}
+
+const fontSizes = [num(), num(), num(), num(), num(), num(), num(), num(), num(), num()];
+
+
+d3
+    .selectAll("p")
+    .data(fontSizes).style("font-size", (d, i) => d + "px")
+    .data([num(), num(), num(), num(), num(), num(), num(), num(), num(), num()]).style("color", d => "hsl(" + Math.random() * 360 + ",100%,50%)")
+    .data(["Brian", "Doris", "Shawn", "Logan", "Mahayla", "Thomas", "Shirley", "Lewis", "Randolph", "Robert", ]).data(["Brian", "Doris", "Shawn", "Thomas", "Mahayla", "Logan", "Shirley", "Allen", "Lewis", "Frankie"]).attr("title", (d) => d);
